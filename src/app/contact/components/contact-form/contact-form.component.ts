aded3567112350d6f93f5083e95ab871d5a00fd1
Hovhannes <hovhannespost@gmail.com>
2020-04-10 17:01:17 +0400

    fix form and add validators

--- src/app/contact/components/contact-form/contact-form.component.ts
+++ src/app/contact/components/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
@@ -7,21 +7,19 @@ import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactFormComponent implements OnInit {
   myForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-  }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.myForm = this.fb.group({
       name: ['Valod', Validators.required],
-      email: ['', Validators.required, Validators.pattern('[a-zA-Z\-0-9]+\.]')],
-      massage: ['', Validators.required, Validators.minLength(20)]
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(20)]],
+      select: ['']
     });
   }
 
-  onSubmit(form: FormGroup) {
-    console.log('Valid?', form.valid); // true or false
-    console.log('Name', form.value.name);
-    console.log('Email', form.value.email);
-    console.log('Message', form.value.message);
+  onSubmit() {
+    const formValues = this.myForm.getRawValue();
+    console.log(formValues)
   }
 }
